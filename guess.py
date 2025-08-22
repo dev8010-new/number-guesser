20cc28105a5aba39ba7cf985fc6493687b9cfd92
Fazliddinbek Xujayev <fazliddinbekxujayev@gmail.com>
2025-08-22 16:59:25 +0300

    add basic random guessser finish

---
+++ guess.py
@@ -0,0 +1,11 @@
+import random
+
+n = random.randint(0,10)
+guess = int(input("guess a number between 0 and 10: ")
+
+if n == guess :
+            print("You got me !")
+else:
+            print("Nah lol")
+
+               
